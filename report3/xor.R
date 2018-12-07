33810181d26b76ac269d83c60ac08a72a001dda4
nat-chan <qzrp01@gmail.com>
2018-12-08 03:05:18 +0900

    10回の結合係数を束ねてヒストグラム説

---
+++ report3/xor.R
@@ -0,0 +1,53 @@
+#!/usr/bin/env R
+#=と<-の使い分け
+#=  : グローバル、(関数内で外側の参照されない?要検証)
+#<- : レキシカルスコープただしfor等はスコープを作らない
+set.seed(0)
+library(nnet)
+library(MASS)
+x1 <- c(0, 0)
+x2 <- c(0, 1)
+x3 <- c(1, 0)
+x4 <- c(1, 1)
+x <- rbind(x1, x2, x3, x4)
+y <- c("-1", "1", "1", "-1")
+xor <- data.frame(x, classes=y)
+#head(xor)
+#mlp <- nnet(classes~., data=xor, size=2, rang=0.5, decay=0, maxit=100)
+#summary(mlp)
+#hist(mlp$wts, breaks=seq(-100, 100,5), freq=TRUE)
+#table(xor$classes,predict(mlp, xor, type="class"))
+
+hidden <- c(1:40)
+iter   <- c(1:10)
+trave  <- rep(0,length(hidden))
+tr     <- matrix(0,length(iter),length(hidden))
+weights <- rep(list(c()), length(hidden))
+for(i in 1:length(hidden)){
+	for(j in 1:length(iter)){
+		print(paste(as.character(i), as.character(j)))
+		model <- nnet(classes~., data=xor, size=hidden[i], rang=0.1)
+		weights[[i]] <- append(weights[[i]], model$wts)
+		out <- predict(model, xor, type="class")
+		tr[j,i] <- mean(out != xor$classes)
+	}
+	trave[i] <- mean(tr[,i])
+}
+
+main <- function(){
+	if(dev.cur() > 1) dev.off() #clf
+	
+	#1. 隠れ素子を一つずつ増やし、10 回の学習で 10 回とも正しく識別できるようになった。隠れ素子の数を求めなさい。 
+	first_match <- which(trave == 0)[1] #複数マッチするので一番初めを取り出す
+	print(first_match)
+	
+	##2. 隠れ素子の数によって誤識別率の平均がどのように変化するかをグラフで示しなさい。
+	#plot(hidden, trave, type="b")
+	##	 , lty=1, lwd=2)
+	#
+	#3. 隠れ素子が 1 個の場合に得られた学習結果について、結合係数の大きさの分布を示しなさい。
+	#hist(weights[[1]], breaks=seq(-100, 100,5), freq=TRUE)
+	
+	#4. 10 回とも正しく識別できた場合の学習結果について、結合係数の大きさの分布を示し、隠れ素子が１個の場合と 比較検討しなさい。
+	hist(weights[[first_match]], freq=TRUE)
+}
